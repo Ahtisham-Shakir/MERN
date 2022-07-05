a5b8980fb98746246d3c24e94736590b06bee010
Ahtisham Shakir <ahtishamshakir777@gmail.com>
2022-07-05 20:08:46 +0500

    setup password hashing

--- server/router/auth.js
+++ server/router/auth.js
@@ -40,12 +40,14 @@ router.post('/register', async (req, res) => {
         const userExist = await User.findOne({ email: email });
         if (userExist) {
             return res.status(422).json({ error: "email already exist" })
+        } else if (password !== cpassword) {
+            return res.status(422).json({ error: "password is not same" })
+        } else {
+            const user = new User({ name, email, phone, work, password, cpassword });
+
+            await user.save()
+            res.status(201).json({ message: "User registered successfully" });
         }
-
-        const user = new User({ name, email, phone, work, password, cpassword });
-
-        await user.save()
-        res.status(201).json({ message: "User registered successfully" });
 
     } catch (error) {
         console.log(error);
